8299fc03534428899767570cfd1ecd23bd845211
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-06-15 15:26:40 -0300

    Need Token to access Home

---
+++ src/Components/Common/UserIsLogged.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect } from 'react'
+import { TokenContext } from '../Context/Token'
+import Router from 'next/router'
+
+export default function UserIsLogged() {
+  const [Token, setToken] = useContext(TokenContext)
+
+  useEffect(() => {
+    if (!Token) {
+      Router.push('/login')
+    }
+  }, [])
+
+  return (
+    <>
+
+    </>
+  )
+}
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 import styled from 'styled-components'
+import UserIsLogged from '../Common/UserIsLogged'
 
 export default function index() {
   return (
     <div>
+      <UserIsLogged />
       HOME
     </div>
   )
